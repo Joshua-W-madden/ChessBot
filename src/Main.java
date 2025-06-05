6fd6d38be876a7ef721e25eba3a162c2e17aa6ff
sonorous3 <88260227+sonorous3@users.noreply.github.com>
2025-06-05 16:02:35 -0400

    Game threads - adds loop tracking/timers implemented Runnable-Run function on game

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,24 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        WindowFrame frame = new WindowFrame();
+
+        JFrame windowFrame = new JFrame();
+        windowFrame.setTitle("Chess");
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setResizable(false);
+
+        ImageIcon image = new ImageIcon("icon.jpg");
+        windowFrame.setIconImage(image.getImage());
+
+        GameWindow GameWindow = new GameWindow();
+        windowFrame.add(GameWindow);
+
+        windowFrame.pack();
+
+        windowFrame.setLocationRelativeTo(null);
+        windowFrame.setVisible(true);
+
+        GameWindow.launchGame();
 
     }
 }
